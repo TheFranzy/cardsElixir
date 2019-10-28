0760dfa52c5e56ba60b0feb0d084d5e6b248c43c
TheFranzy <galen.housego@gmail.com>
2019-10-28 16:13:57 -0500

    added documentation on different functions

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,10 @@ defmodule Cards do
   @moduledoc """
   Provides methodsr creating and handling a deck of cards
   """
+
+  @doc """
+  Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", 
     "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
@@ -13,23 +17,38 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  Returns a list of strings representing a deck of playing cards in a shuffled order
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Returns a boolean indicating if the deck of cards contains the specified string
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Returns a specified number of strings representing a hand of playing cards
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+  Saves a file containing a list representing a deck of playing cards
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+  Loads a saved list representing a deck of playing cards
+  """
   def load(filename) do
 
     case File.read(filename) do
@@ -39,6 +58,9 @@ defmodule Cards do
 
   end
 
+  @doc """
+  Returns a list of a specific number of strings representing playing cards
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
